1930d251f054e9f782abe2f669667e61da2f1c92
pearlDingzhen <tianxj15@tsinghua.org.cn>
2026-01-01 09:58:02 +0800

    feat: Complete refactor - clean up old code and add new framework
    - Remove legacy workflow files
    - Update .gitignore to exclude source_code_of_extern
    - Finalize multiscale2 package structure

--- multiscale2/__init__.py
+++ multiscale2/__init__.py
@@ -1,1 +1,42 @@
-# This file makes 'multiscale2' a Python package.
+#!/usr/bin/env python3
+"""
+Multiscale2 Package
+
+A multi-stage workflow for simulating protein condensates.
+
+Subpackages:
+- src: Coarse-grained simulation framework
+- extern: External packages (ms2_calvados, etc.)
+"""
+
+from .src import (
+    CGSimulationConfig,
+    CGComponent,
+    ComponentType,
+    TopologyType,
+    ComputePlatform,
+    SimulationParams,
+    SimulationResult,
+    CGSimulator,
+    CalvadosWrapper,
+    run_calvados,
+)
+
+__all__ = [
+    # Configuration
+    'CGSimulationConfig',
+    'CGComponent',
+    'ComponentType',
+    'TopologyType',
+    'ComputePlatform',
+    'SimulationParams',
+    'SimulationResult',
+    
+    # Simulator
+    'CGSimulator',
+    
+    # CALVADOS wrapper
+    'CalvadosWrapper',
+    'run_calvados',
+]
+
--- setup.py
+++ setup.py
@@ -5,7 +5,16 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     description='A multi-stage workflow for simulating protein condensates.',
-    author='Xiaojing Hazard Tian',
+    author='Xiaojing Tian',
     author_email='tianxj15@tsinghua.org',
+    entry_points={
+        'console_scripts': [
+            'ms2=multiscale2.cli:main',
+        ],
+    },
+    install_requires=[
+        'click>=8.0',
+        'pyyaml>=6.0',
+    ],
 )
 
